3c76a3c88fa504eb340d1cc09e6aaf5d22d57ad9
ysphdnine <104703824+ysphdnine@users.noreply.github.com>
2022-05-01 21:52:09 +0900

    アロー関数

--- src/index.js
+++ src/index.js
@@ -48,14 +48,34 @@
 /**
  * テンプレート文字列
  */
-const name = "じゃけぇ";
-const age = 28;
-// 「私の名前はじゃけぇです。年齢は28歳です。」
+// const name = "じゃけぇ";
+// const age = 28;
+// // 「私の名前はじゃけぇです。年齢は28歳です。」
 
-// 従来の方法
-const message1 = "私の名前は" + name + "です。年齢は" + age + "です。";
-console.log(message1);
+// // 従来の方法
+// const message1 = "私の名前は" + name + "です。年齢は" + age + "です。";
+// console.log(message1);
 
-// テンプレート文字列を用いた方法
-const message2 = `私の名前は${name}です。年齢は${age}です。`;
-console.log(message2);
+// // テンプレート文字列を用いた方法
+// const message2 = `私の名前は${name}です。年齢は${age}です。`;
+// console.log(message2);
+
+/**
+ * アロー関数
+ */
+// 従来の関数
+// function func1(str) {
+//   return str;
+// }
+const func1 = function (str) {
+  return str;
+};
+console.log(func1("func1です"));
+
+// アロー関数
+const func2 = (str) => str;
+console.log(func2("func2です"));
+
+const func3 = (num1, num2) => num1 + num2;
+
+console.log(func3(10, 20));
