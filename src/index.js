a03beea314f1136c1188b0b5cdfaa0797ed9eccb
ysphdnine <104703824+ysphdnine@users.noreply.github.com>
2022-05-01 21:42:12 +0900

    テンプレート文字列

--- src/index.js
+++ src/index.js
@@ -40,7 +40,22 @@
 // console.log(val4);
 
 // constで定義した配列はプロパティの変更が可能
-const val5 = ["dog", "cat"];
-val5[0] = "bird";
-val5.push("monkey");
-console.log(val5);
+// const val5 = ["dog", "cat"];
+// val5[0] = "bird";
+// val5.push("monkey");
+// console.log(val5);
+
+/**
+ * テンプレート文字列
+ */
+const name = "じゃけぇ";
+const age = 28;
+// 「私の名前はじゃけぇです。年齢は28歳です。」
+
+// 従来の方法
+const message1 = "私の名前は" + name + "です。年齢は" + age + "です。";
+console.log(message1);
+
+// テンプレート文字列を用いた方法
+const message2 = `私の名前は${name}です。年齢は${age}です。`;
+console.log(message2);
