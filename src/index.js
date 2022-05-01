5f10315b33170af1569062e07453b1a30b1e1c74
ysphdnine <104703824+ysphdnine@users.noreply.github.com>
2022-05-01 22:04:03 +0900

    デフォルト値、引数など

--- src/index.js
+++ src/index.js
@@ -96,11 +96,18 @@
 //  const message2 = `名前は${name}です。年齢は${age}歳です。`;
 //  console.log(message2);
 
-const myProfile = ["じゃけぇ", 28];
+// const myProfile = ["じゃけぇ", 28];
 
-const message3 = `私の名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`;
-console.log(message3);
+// const message3 = `私の名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`;
+// console.log(message3);
 
-const [name, age] = myProfile;
-const message4 = `私の名前は${name}です。年齢は${age}歳です。`;
-console.log(message4);
+// const [name, age] = myProfile;
+// const message4 = `私の名前は${name}です。年齢は${age}歳です。`;
+// console.log(message4);
+
+/**
+ * デフォルト値、引数など
+ */
+
+const sayHello = (name = "ゲスト") => console.log(`こんにちは！${name}さん！`);
+sayHello("じゃけぇ");
