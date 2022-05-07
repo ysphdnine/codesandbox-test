d52b71bd2e8b420f832f8cf92ca7d3d09f91fbfc
ysphdnine <104703824+ysphdnine@users.noreply.github.com>
2022-05-07 10:36:24 +0900

    論理演算子の本当の意味

--- src/index.js
+++ src/index.js
@@ -150,7 +150,7 @@
 /**
  * mapやfilterを使った配列の処理
  */
-const nameArr = ["田中", "山田", "じゃけぇ"];
+// const nameArr = ["田中", "山田", "じゃけぇ"];
 // for (let index = 0; index < nameArr.length; index++) {
 //   console.log(`${index + 1}番目は${nameArr[index]}`);
 // }
@@ -190,7 +190,30 @@ const nameArr = ["田中", "山田", "じゃけぇ"];
 // const formattedNum = typeof num === 'number' ? num.toLocaleString() : '数値を入力してください';
 // console.log(formattedNum);
 
-const checkSum = (num1, num2) => {
-  return num1 + num2 > 100 ? "100を超えています" : "許容範囲内です";
-};
-console.log(checkSum(50, 40));
+// const checkSum = (num1, num2) => {
+//   return num1 + num2 > 100 ? "100を超えています" : "許容範囲内です";
+// };
+// console.log(checkSum(50, 40));
+
+/**
+ * 論理演算子の本当の意味を知ろう　&& ||
+ */
+// const flag1 = true;
+// const flag2 = false;
+
+// if (flag1 || flag2) {
+//   console.log("１か２はtrueになります");
+// }
+// if (flag1 && flag2) {
+//   console.log("１か２はtrueになります");
+// }
+
+// || は左側がfalseなら右側を返す
+const num = 100;
+const fee = num || "金額未設定です";
+console.log(fee);
+
+// && は左側がtrueなら右側を返す
+const num2 = null;
+const fee2 = num2 && "何か設定されました";
+console.log(fee2);
